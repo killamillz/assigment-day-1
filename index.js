71568267b56811ea5bdad7556f3a957e8ddd5e4a
InnoIbeto <114240719+InnoIbeto@users.noreply.github.com>
2026-08-17 15:10:45 +0100

    My solution to the task

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 3. your favorite programming language - A string indicating your preferred programming language
 */
 
-// Alozie bright
+// Innocent Ibeto
 
 // Esther
 
@@ -25,3 +25,15 @@ let e_fun_thing = 'I love art'
 let fav_prog_lang = 'C'
 
 console.log(`My name is ${emmanuels_name} ${emmanuels_surname}, a fun thing about me is that ${e_fun_thing}, and my favourite programming language is ${fav_prog_lang}.`)
+let FirstName  = "Innocent";
+
+let LastName  = "Ibeto";
+
+let FullName = FirstName + " " + LastName;
+
+const FunThingAboutMe = `Wagwan class, i am ${FullName}, and one fun thing about me is I AM BATMAN!, Shhh don't tell anyone`
+
+let FavLanguage = "Ruby on Rails"
+
+console.log(FunThingAboutMe);
+console.log(FavLanguage);
