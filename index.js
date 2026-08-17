7931e6317b4d22b9a78c699479062564a2c272ef
Ngozichukwuka-peace <ngozichukwukapeace@gmail.com>
2026-08-17 15:51:24 +0100

    Peace submission

--- index.js
+++ index.js
@@ -53,7 +53,13 @@ console.log(FunThingAboutMe);
 console.log(FavLanguage);
 
 
+//Peace Ngozichukwuka
+let FirstName = "Peace";
+let LastName ="Ngozichukwuka";
+let Fun = "Watching crime shows";
+let FavLanguage = "Javascript";
 
+console.log(`${FirstName} ${LastName} ${Fun} ${FavLanguage}`);
 
 
 
