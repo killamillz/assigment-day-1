2f744a4bcec6809dac6a5fa33fc6c87df8832df4
killamillz <killamillz0@gmail.com>
2026-08-17 14:32:31 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Clone repo and open
+//  In the index.js file, console.log
+/* 
+1. your name - make your name and surname a variable then display both of them on the terminal
+2. one fun thing about yourself - A short string about yourself 
+3. your favorite programming language - A string indicating your preferred programming language
+*/
+
+// Alozie bright
+
+// Esther
