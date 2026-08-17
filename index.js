a7313bc965c18260d1098a7a675e8917b9f604dd
Esther Idowu <idowuesther1234@gmail.com>
2026-08-17 15:02:06 +0100

    First Commit

--- index.js
+++ index.js
@@ -9,3 +9,8 @@
 // Alozie bright
 
 // Esther
+let name = "Esther";
+let surname = "Idowu";
+console.log(name + " " + surname);
+console.log("I love to dance!");
+console.log("Python is my favorite programming language.");
