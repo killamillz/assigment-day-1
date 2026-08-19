be4f90caba6560cdd2db731eba557c4563de6d4b
Chidera Nweke <thatguyspiff@gmail.com>
2026-08-19 15:58:36 +0100

    completed

---
+++ classworkDay2.js
@@ -0,0 +1,13 @@
+var a=10;
+const b= "5";
+var c = true;
+const d = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * d);
+console.log(b + d);
+console.log(a + c);
+console.log(a == "10");
+console.log(a === "10")
+console.log(!c);
